fa850a94750e2204451d9753040f2de1bcbb71af
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-14 12:25:31 +0100

    Add files via upload

---
+++ Cwiczenia 4 Zad 6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Cwiczenia_4_Zad_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = 1, m;
+
+            while (n <= 4)
+            {
+                m = 1;
+                while (m <= 4)
+                {
+                    if (n == 1 | n == 4 | m == 1 | m == 4) 
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                    m++;
+                }
+                Console.WriteLine();
+                n++;
+            }
+
+        }
+    }
+}
